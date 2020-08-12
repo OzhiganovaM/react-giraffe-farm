1ae401b4b8e7a9947e334257bb5151bd14f2acf5
OzhiganovaM <oziganova96@gmail.com>
2020-08-12 19:42:03 +0300

    Added custom scroll

--- src/Giraffes.js
+++ src/Giraffes.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import {ReactComponent as Close} from './Assets/icons/close.svg';
 import GiraffeCard from './GiraffeCard';
 import GiraffeCardActive from './GiraffeCardActive';
@@ -6,11 +6,16 @@ import data from './Data.json'
 import {ReactComponent as Add} from './Assets/icons/add.svg';
 import GiraffesMenu from './GiraffesMenu';
 
-export default function Giraffes () {
+let isInit = false
 
+export default function Giraffes () {    
+    let percentValue = '50%'
+
+  // logic for adding new cards
     const [newCards1, setNewCards1] = useState(0);
     function addNewCard1 () {
         setNewCards1(newCards1 + 1)
+        percentValue = percentValue + 12.5 + '%'
     }
     
     const [newCards2, setNewCards2] = useState(0);
@@ -21,28 +26,107 @@ export default function Giraffes () {
     const [newCards3, setNewCards3] = useState(0);
     function addNewCard3 () {
         setNewCards3(newCards3 + 1)
+
     }
+
+    // logic for % in progress bar
+    //  useEffect (() => {
+        //  let cards = document.querySelectorAll('.card')
+        // let cardsAmount = Array.from(cards)
+        // console.log(cardsAmount.length)
+    // })
+    
+    // Scroll logic
+
+    function updateScroll() {
+        let maxScrollTranslate = document.querySelector('.giraffe-scroll-content').scrollWidth - document.querySelector('.giraffe-scroll-content').offsetWidth
+
+        document.querySelector('#slider').style.width = maxScrollTranslate < (document.querySelector('#scrollbar').offsetWidth - 20) ? document.querySelector('#scrollbar').offsetWidth - maxScrollTranslate + 'px' : '20px'
+        document.querySelector('#scrollbar').style.visibility = maxScrollTranslate <= 1 ? 'hidden' : 'visible'
+    }
+
+    useEffect(()=>{
+        updateScroll()
+
+    //  next 2 lines - not to make action repeat
+      if ( isInit ) return
+      isInit = true
+      let isDrag = false
+      let previousX = 0
+      let slider = document.querySelector('#slider')
+      
+      let content = document.querySelector('.giraffe-scroll-content')
+
+      slider.addEventListener('mousedown', e=>{
+        isDrag = true
+        previousX = e.x
+      })
+
+      document.addEventListener('mouseup', e=>{
+        isDrag = false
+      })
+
+      document.addEventListener('mousemove', e=>{
+        if (isDrag) {
+          // disable another mouse activity
+          e.preventDefault()
+          e.stopPropagation()
+
+          let maxWidth = document.querySelector('#scrollbar').offsetWidth - slider.offsetWidth - 4
+
+          if ( e.x > previousX ) {
+            // move slider right
+            let delta = (e.x - previousX)
+            let currentLeftValue = slider.style.left ? parseInt(slider.style.left) : 0
+            let newLeftValue = currentLeftValue + delta
+            if (newLeftValue > maxWidth) newLeftValue = maxWidth
+
+            slider.style.left = newLeftValue  + 'px'
+            previousX = e.x
+          } else if ( e.x < previousX ) {   
+            // move slider left 
+            let delta = (previousX - e.x)
+            let currentLeftValue = slider.style.left ? parseInt(slider.style.left) : 0
+            let newLeftValue = currentLeftValue - delta
+            if ( newLeftValue < 0 ) newLeftValue = 0
+
+            slider.style.left = newLeftValue + 'px'
+            previousX = e.x
+          }
+          // move content
+          let maxScrollTranslate = document.querySelector('.giraffe-scroll-content').scrollWidth - document.querySelector('.giraffe-scroll-content').offsetWidth
+          let leftValue = parseInt(slider.style.left)/maxWidth
+          content.style.transform = 'translateX(' + (-maxScrollTranslate*leftValue) + 'px)'
+          // change slider width
+          updateScroll()
+        }
+      })
+    })
 
     return(
     <div className="giraffes_container">
         <GiraffesMenu />
         <div className="giraffes_wrapper">
+          <div id="anchor"/>
             <div className="tab active" id="tab1">
                 <div className="giraffe-window_header">
                     <h1>Жирафы</h1>
                     <button onClick={addNewCard1}><Add className="giraffe-window_header__add-new"/></button>
                 </div>
-                <div className="giraffe-window__wrapper">
+                <div className="giraffe-scroll-content">
                     {Array(newCards1).fill(<GiraffeCardActive />)}
                     {data.cards.map((card, key) =>{
                         return <GiraffeCard card={card} key={key} />
                     }
                     )}
                 </div>
+                <div id="scrollbar">
+                    <span id="slider" />
+                </div>
                 <div className="info-section">
                     <Close className="close-icon"/>
                     <div className="info-section_progress-percent">
-                        <p className="info-section_percent">75%</p>
+                        <p className="info-section_percent">{percentValue}</p>
                         <p>Заполнение вольера</p>
                     </div>
                     <div className="info-section_progress-bar">
